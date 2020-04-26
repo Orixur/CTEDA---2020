7ec1ef7d8c0fab68eabcb99d777c393ddf33f02b
Orixur <agustin.gimenez1996@gmail.com>
2020-04-26 20:31:52 -0300

    include and level function for ArbolGeneral, and custom basic implementation Cola of a queue

--- practica1/arbol_general.py
+++ practica1/arbol_general.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from .nodo_general import NodoGeneral
+from .cola import Cola
 
 class ArbolGeneral(object):
     def __init__(self, root: NodoGeneral = None):
@@ -33,12 +34,51 @@ class ArbolGeneral(object):
         return self.root != None and not self.childs
 
     def height(self) -> int:
+        """
+            Esta función se encarga de hacer un recorrido preorden, para encontrar la altura (= height = h) para la instancia del árbol
+        """
         if self.isLeaf:
             return 0 # Si quisiera contar por nodos y no por aristas, debería devolver 1 cuando encuentro una arista
         heights = []
         for c in self.childs:
             heights.append(c.height())
         return 1 + max(heights)
-    
-    def level(self) -> int:
-        pass
+
+    def include(self, data) -> bool:
+        """
+            Esta función chequea si un valor se haya en sus sub árboles. Esta función NO verifica por posibles datos duplicados, se
+            asume que la estructura de datos no almacena duplicados.
+        """
+        if self.root_data == data:
+            return self
+        matching_tree = None
+        for c in self.childs:
+            matching_tree = c.include(data=data)
+            if matching_tree is not None:
+                break
+        return matching_tree
+
+    def level(self, data) -> int:
+        """
+            Esta función encontrará la distancia entre la Raiz y el Dato provisto como parámetro. El calculo se realiza contando arista.
+        """
+        matching_tree = self.include(data=data)
+        if not matching_tree:
+            return
+        if matching_tree == self:
+            return 0
+        q = Cola()
+        q.put(self.root)
+        level = 0
+        while not q.isEmpty:
+            current_node = q.get()
+            if current_node is None:
+                level += 1
+                continue
+            elif current_node.data == data:
+                break
+            for child in current_node.childs:
+                q.put(child)
+            q.put(None) # Sentinel value, marca de fin de nivel
+        
+        return level
--- practica1/tests/test_arbol_general.py
+++ practica1/tests/test_arbol_general.py
@@ -9,6 +9,48 @@ class ArbolGeneralTest(unittest.TestCase):
         tree = ArbolGeneral(root=root_node)
         return root_node, tree
 
+    def build_tree(self):
+        node_1 = NodoGeneral('1')
+        tree = ArbolGeneral(node_1)
+
+        node_2 = NodoGeneral('2')
+        node_3 = NodoGeneral('3')
+        node_4 = NodoGeneral('4')
+        node_5 = NodoGeneral('5')
+        node_6 = NodoGeneral('6')
+        
+        node_1.add_child(node_2)
+        node_1.add_child(node_3)
+        node_2.add_child(node_4)
+        node_4.add_child(node_5)
+        node_3.add_child(node_6)
+
+        return tree
+
+    def build_big_tree(self):
+        node_1 = NodoGeneral('1')
+        tree = ArbolGeneral(node_1)
+
+        node_2 = NodoGeneral('2')
+        node_3 = NodoGeneral('3')
+        node_4 = NodoGeneral('4')
+        node_5 = NodoGeneral('5')
+        node_6 = NodoGeneral('6')
+        node_7 = NodoGeneral('7')
+        node_8 = NodoGeneral('8')
+        node_8 = NodoGeneral('9')
+        
+        node_1.add_child(node_2)
+        node_1.add_child(node_3)
+        node_2.add_child(node_4)
+        node_4.add_child(node_5)
+        node_3.add_child(node_6)
+        node_5.add_child(node_7)
+        node_5.add_child(node_8)
+        node_5.add_child(node_9)
+
+        return tree
+
     def test_trees_can_be_created_without_root(self):
         tree = ArbolGeneral()
 
@@ -76,19 +118,23 @@ class ArbolGeneralTest(unittest.TestCase):
         self.assertTrue(tree.isLeaf)
 
     def test_tree_height_equal_the_distance_to_the_farest_leaf_counting_edges(self):
-        node_1 = NodoGeneral('1')
-        tree = ArbolGeneral(node_1)
-
-        node_2 = NodoGeneral('2')
-        node_3 = NodoGeneral('3')
-        node_4 = NodoGeneral('4')
-        node_5 = NodoGeneral('5')
-        node_6 = NodoGeneral('6')
-        
-        node_1.add_child(node_2)
-        node_1.add_child(node_3)
-        node_2.add_child(node_4)
-        node_4.add_child(node_5)
-        node_3.add_child(node_6)
+        tree = self.build_tree()
         
         self.assertEqual(tree.height(), 3)
+
+    def test_tree_finds_for_a_value_in_sub_trees(self):
+        tree = self.build_tree()
+
+        matching_tree = tree.include(data='5')
+        self.assertEqual(matching_tree.root_data, '5')
+
+    def test_tree_cannot_find_a_value_that_not_exist(self):
+        tree = self.build_tree()
+
+        matching_tree = tree.include(data="i don't exist!")
+        self.assertIsNone(matching_tree)
+
+    def test_can_know_level_for_given_value(self):
+        tree = self.build_tree()
+
+        self.assertEqual(tree.level(data='5'), 3)
