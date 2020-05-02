5a712ca2abbeff93dcdfd127d274bb5d6c6650ee
Orixur <agustin.gimenez1996@gmail.com>
2020-05-02 15:21:34 -0300

    Binary Tree with basic methods and attrs, traverse methods and unit testing for these features

---
+++ practica2/binary_tree.py
@@ -0,0 +1,106 @@
+from .binary_node import BinaryNode
+from practica1.cola import Cola
+
+class BinaryTree(object):
+    def __init__(self, root: BinaryNode):
+        self._root = root
+
+    @property
+    def root(self):
+        return self._root
+
+    @root.setter
+    def root(self, new_root):
+        self._root = new_root
+
+    @property
+    def root_data(self):
+        return self.root.data
+
+    @property
+    def leftChild(self):
+        if self.root.leftChild is None:
+            return None
+        return BinaryTree(self.root.leftChild)
+        
+    @property
+    def rightChild(self):
+        if self.root.rightChild is None:
+            return None
+        return BinaryTree(self.root.rightChild)
+
+    @leftChild.setter
+    def leftChild(self, new_child: 'BinaryTree'):
+        if new_child is None:
+            self.root.leftChild = None
+        else:
+            self.root.leftChild = new_child.root
+        
+    @rightChild.setter
+    def rightChild(self, new_child: 'BinaryTree'):
+        if new_child is None:
+            self.root.rightChild = None
+        else:
+            self.root.rightChild = new_child.root
+
+    @property
+    def isLeaf(self):
+        return self.root is not None and self.root.rightChild is None and self.root.leftChild is None
+
+    @property
+    def isEmpty(self):
+        return self.root == None
+
+    def inorder(self):
+        if not self.isEmpty and self.isLeaf:
+            return [self.root_data]
+        traverse = []
+        traverse += self.leftChild.inorder()
+        traverse += [self.root_data]
+        traverse += self.rightChild.inorder()
+
+        return traverse
+
+    def preorder(self):
+        if not self.isEmpty and self.isLeaf:
+            return [self.root_data]
+        traverse = []
+        traverse += [self.root_data]
+        traverse += self.leftChild.preorder()
+        traverse += self.rightChild.preorder()
+
+        return traverse
+    
+    def postorder(self):
+        if not self.isEmpty and self.isLeaf:
+            return [self.root_data]
+        traverse = []
+        traverse += self.leftChild.postorder()
+        traverse += self.rightChild.postorder()
+        traverse += [self.root_data]
+
+        return traverse
+
+    def level_traverse(self):
+        if not self.isEmpty and self.isLeaf:
+            return self.root_data
+        q = Cola()
+        q.put(self.root)
+        q.put(None)
+        level = 0
+        traverse = []
+        while not q.isEmpty:
+            current_node = q.get()
+            if current_node is None:
+                level += 1
+                if q.isEmpty:
+                    break
+                q.put(None)
+                continue
+            traverse.append(current_node.data)
+            if current_node.leftChild is not None:
+                q.put(current_node.leftChild)
+            if current_node.rightChild is not None:
+                q.put(current_node.rightChild)
+
+        return traverse, level
---
+++ practica2/tests/test_binaryTree.py
@@ -0,0 +1,85 @@
+import unittest
+
+from practica2.binary_tree import BinaryTree
+from practica2.binary_node import BinaryNode
+
+class BinaryTreeTest(unittest.TestCase):
+    def build_basic_tree(self):
+        root = BinaryNode('root-node')
+        tree = BinaryTree(root)
+        left_child = BinaryTree(BinaryNode('left-child'))
+        right_child = BinaryTree(BinaryNode('right-child'))
+
+        tree.leftChild = left_child
+        tree.rightChild = right_child
+
+        return tree, left_child.root, right_child.root
+
+    def build_mid_tree(self):
+        node_1 = BinaryNode(1)
+        tree = BinaryTree(node_1)
+        node_2 = BinaryTree(BinaryNode(2))
+        node_3 = BinaryTree(BinaryNode(3))
+        node_4 = BinaryTree(BinaryNode(4))
+        node_5 = BinaryTree(BinaryNode(5))
+        node_6 = BinaryTree(BinaryNode(6))
+        node_7 = BinaryTree(BinaryNode(7))
+
+        tree.leftChild = node_2
+        tree.rightChild = node_3
+        node_2.leftChild = node_4
+        node_2.rightChild = node_5
+        node_5.leftChild = node_6
+        node_5.rightChild = node_7
+
+        return tree
+
+    def test_cannot_instantiate_binary_tree_without_root(self):
+        with self.assertRaises(TypeError) as context:
+            tree = BinaryTree()
+
+    def test_can_set_and_access_root_childs(self):
+        tree, left_child, right_child = self.build_basic_tree()
+
+        self.assertEqual(tree.leftChild.root, left_child)
+        self.assertEqual(tree.rightChild.root, right_child)
+
+    def test_tree_can_delete_root_childs(self):
+        tree, left_child, right_child = self.build_basic_tree()
+
+        tree.leftChild = None
+        self.assertEqual(tree.leftChild, None)
+
+        tree.rightChild = None
+        self.assertEqual(tree.rightChild, None)
+
+    def test_tree_is_leaf_when_root_doesnot_have_any_child(self):
+        root = BinaryNode('leaf-root')
+        tree = BinaryTree(root)
+
+        self.assertTrue(tree.isLeaf)
+
+    def test_inorder_traversal(self):
+        tree = self.build_mid_tree() # [1, 2, 3, 4, 5, 6, 7]
+
+        traverse = tree.inorder()
+        self.assertListEqual(traverse, [4, 2, 6, 5, 7, 1, 3])
+
+    def test_preorden_traversal(self):
+        tree = self.build_mid_tree() # [1, 2, 3, 4, 5, 6, 7]
+
+        traverse = tree.preorder()
+        self.assertListEqual(traverse, [1, 2, 4, 5, 6, 7, 3])
+
+    def test_postorder_traversal(self):
+        tree = self.build_mid_tree() # [1, 2, 3, 4, 5, 6, 7]
+
+        traverse = tree.postorder()
+        self.assertListEqual(traverse, [4, 6, 7, 5, 2, 3, 1])
+
+    def test_level_traverse(self):
+        tree = self.build_mid_tree() # [1, 2, 3, 4, 5, 6, 7]
+
+        traverse, level = tree.level_traverse()
+        self.assertListEqual(traverse, [1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(level, 4)
