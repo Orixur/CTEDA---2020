4d5965dd859c07879b4161b5aed8fc270b7051b5
Orixur <agustin.gimenez1996@gmail.com>
2020-05-14 19:12:33 -0300

    deleteMin and insert heap

--- practica2/tests/test_heap.py
+++ practica2/tests/test_heap.py
@@ -11,3 +11,60 @@ class MinHeapTest(unittest.TestCase):
 
         self.assertEqual(recorrido, items)
         self.assertEqual(nivel, 3)
+    
+    def test_can_insert_new_item(self):
+        heap = MinHeap()
+        
+        heap.insert(1)
+        heap.insert(2)
+        heap.insert(3)
+
+        self.assertListEqual(heap.data, [1,2,3])
+
+    def test_heap_filter_up_after_insertion(self):
+        heap = MinHeap()
+        
+        heap.insert(4)
+        self.assertListEqual(heap.data, [4])
+        heap.insert(3)
+        self.assertListEqual(heap.data, [3,4])
+        heap.insert(2)
+        self.assertListEqual(heap.data, [2,4,3])
+        heap.insert(1)
+        self.assertListEqual(heap.data, [1,2,3,4])
+
+    def test_heap_filter_down_after_deletion(self):
+        heap = MinHeap(data=[1,2,3,4])
+
+        heap.deleteMin()
+
+        self.assertListEqual(heap.data, [2,4,3])
+
+    def test_ejercicio_6(self):
+        heap = MinHeap()
+        heap.insert(50)
+        self.assertListEqual(heap.data, [50])
+        heap.insert(52)
+        self.assertListEqual(heap.data, [50, 52])
+        heap.insert(41)
+        self.assertListEqual(heap.data, [41, 52, 50])
+        heap.insert(54)
+        self.assertListEqual(heap.data, [41, 52, 50, 54])
+        heap.insert(46)
+        self.assertListEqual(heap.data, [41, 46, 50, 54, 52])
+
+        heap.deleteMin()
+        self.assertListEqual(heap.data, [46, 52, 50, 54])
+        heap.deleteMin()
+        self.assertListEqual(heap.data, [50, 52, 54])
+        heap.deleteMin()
+        self.assertListEqual(heap.data, [52, 54])
+
+        heap.insert(45)
+        self.assertListEqual(heap.data, [45, 54, 52])
+        heap.insert(48)
+        self.assertListEqual(heap.data, [45, 48, 52, 54])
+        heap.insert(55)
+        self.assertListEqual(heap.data, [45, 48, 52, 54, 55])
+        heap.insert(43)
+        self.assertListEqual(heap.data, [43, 48, 45, 54, 55, 52])
